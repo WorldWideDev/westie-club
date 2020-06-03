f57851e343fb7077e5e6bacd7c6c38357243f902
= <=>
2020-06-03 16:50:40 -0700

    updated content

--- Models/Referral.cs
+++ Models/Referral.cs
@@ -5,35 +5,35 @@ namespace WestieClub.Models
     public class Referral
     {
         public static Referral Sup;
-        [Display(Name="First Name*")]
+        [Display(Name="First Name")]
         [Required]
         public string FirstName {get;set;}
         [Display(Name="Last Name")]
         public string LastName {get;set;}
         [Required]
-        [Display(Name="City*")]
+        [Display(Name="City")]
         public string City {get;set;}
         [Required]
-        [Display(Name="State*")]
+        [Display(Name="State")]
         public string State {get;set;}
         [Required]
         [EmailAddress]
-        [Display(Name="Email Address*")]
+        [Display(Name="Email Address")]
         public string Email {get;set;}
         [Required]
         [EmailAddress]
         [Compare("Email")]
-        [Display(Name="Repeat Email Address*")]
+        [Display(Name="Repeat Email Address")]
         public string EmailConfirm {get;set;}
         [Required]
-        [Display(Name="Desired Gender*")]
+        [Display(Name="Desired Gender")]
         public string Gender {get;set;}
         public enum Genders
         {
             Male = 0, Female = 1, Either = 3
         };
         [Required]
-        [Display(Name="Puppy or Adult*")]
+        [Display(Name="Puppy or Adult")]
         public string DesiredAge {get;set;}
         public enum Ages
         {
